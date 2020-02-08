3ec6dc9d57c3916946985c8a0e7d7b672726aede
kainhuck <kainhuck@163.com>
2020-02-08 15:02:09 +0800

    day_24

--- day_24/gin_api_demo/apps/memeber.go
+++ day_24/gin_api_demo/apps/memeber.go
@@ -5,30 +5,136 @@
 package apps
 
 import (
+	"go_learn/day_24/gin_api_demo/models"
+	"log"
+	"net/http"
+	"strconv"
+
 	"github.com/gin-gonic/gin"
 )
 
 // AddUserHandler ...
 func AddUserHandler(c *gin.Context) {
+	// 初始化一个对象
+	m := new(models.Member)
+	m.Username = c.Request.FormValue("username")
+	m.Password = c.Request.FormValue("password")
 
+	if id, err := m.AddMember(); err != nil {
+		// 失败
+		c.JSON(http.StatusExpectationFailed, gin.H{
+			"status":  http.StatusBadRequest,
+			"message": err.Error(),
+			"data":    "",
+		})
+		log.Fatal(err.Error())
+	} else {
+		m.ID = uint(id)
+		c.JSON(http.StatusOK, gin.H{
+			"status":  http.StatusOK,
+			"message": "SUCCESS",
+			"data":    m,
+		})
+
+	}
 }
 
 // GetOneMemHandler ...
 func GetOneMemHandler(c *gin.Context) {
-
+	mid, _ := strconv.Atoi(c.Param("id"))
+	mem, err := models.OneMember(mid)
+	if err != nil {
+		// 失败
+		c.JSON(http.StatusExpectationFailed, gin.H{
+			"status":  http.StatusBadRequest,
+			"message": err.Error(),
+			"data":    "",
+		})
+		log.Fatal(err.Error())
+	} else {
+		c.JSON(http.StatusOK, gin.H{
+			"status":  http.StatusOK,
+			"message": "SUCCESS",
+			"data":    mem,
+		})
+	}
 }
 
 // GetMemListHandler ...
 func GetMemListHandler(c *gin.Context) {
+	filters := make([]interface{}, 0)
+	filters = append(filters, "id", "<>", "0")
+
+	page, _ := strconv.Atoi(c.Request.FormValue("page"))
+	pageSize, _ := strconv.Atoi(c.Request.FormValue("page_size"))
+
+	if page == 0 {
+		page = 1
+	}
+	if pageSize == 0 {
+		pageSize = 10
+	}
+
+	list, count, err := models.ListMember(page, pageSize, filters...)
+
+	if err != nil {
+		c.JSON(http.StatusExpectationFailed, gin.H{
+			"status":  http.StatusExpectationFailed,
+			"message": err.Error(),
+			"data":    "",
+		})
+		log.Fatal(err.Error())
+	} else {
+		c.JSON(http.StatusOK, gin.H{
+			"status":    http.StatusOK,
+			"message":   "SUCCESS",
+			"data":      list,
+			"count":     count,
+			"page_size": pageSize,
+			"current":   page,
+		})
+	}
 
 }
 
 // DeleteMemHandler ...
 func DeleteMemHandler(c *gin.Context) {
+	mid, _ := strconv.Atoi(c.Param("id"))
 
+	if n, err := models.DeleteMember(mid); err != nil {
+		c.JSON(http.StatusExpectationFailed, gin.H{
+			"status":  http.StatusBadRequest,
+			"message": err.Error(),
+			"data":    "",
+		})
+		log.Fatal(err.Error())
+	} else {
+		c.JSON(http.StatusOK, gin.H{
+			"status":  http.StatusOK,
+			"message": "SUCCESS",
+			"data":    n,
+		})
+	}
 }
 
 // EditMemHandler ...
 func EditMemHandler(c *gin.Context) {
-
+	mid, _ := strconv.Atoi(c.Param("id"))
+	m := new(models.Member)
+	m.Username = c.Request.FormValue("username")
+	m.Password = c.Request.FormValue("password")
+	if n, err := m.UpdateMember(mid); err != nil {
+		c.JSON(http.StatusExpectationFailed, gin.H{
+			"status":  http.StatusBadRequest,
+			"message": err.Error(),
+			"data":    "",
+		})
+		log.Fatal(err.Error())
+	} else {
+		c.JSON(http.StatusOK, gin.H{
+			"status":  http.StatusOK,
+			"message": "SUCCESS",
+			"data":    n,
+		})
+	}
 }
